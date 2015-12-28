f66d19509b511f8993edffdbdc15b2acc13ad770
stelson <sthelmo@gmail.com>
2015-12-28 21:59:00 +0100

    Better initial parameters using Tuner.

--- Plant1delay.m
+++ Plant1delay.m
@@ -1,20 +1,29 @@
 clear all; 
 clc;
 % 500, 1000 - Max 20, x0 = 500*ones
+% 1000 x0 = [170; 0.2; -518; 0.32];
+% 500 x0 = [148; 0.35; -259; 0.57];
 
 T1 = [0.02 0.05 0.1 0.2 0.3 0.5 0.7 1 1.3 1.5 2 4 6 8 10 20 50 100 200 500 1000];
-options = optimset('MaxFunEvals', 5);
+options = optimset('MaxFunEvals', 20);
 
-for i = 20:21;
+for i = 1:21;
     
     num = [1];
     denum = [T1(i) 1];
     G = tf(num, denum, 'InputDelay', 1);
     
-    if T1(i) > 100
-        x0 = 500*ones(4, 1);
-    else
+    
+    if T1(i) == 500
+        x0 = [148; 0.35; -259; 0.57];
+    elseif T1(i) == 1000
+        x0 = [170; 0.2; -518; 0.32];
+    elseif T1(i) > 0 && T1(i) < 1
         x0 = 1*ones(4, 1);
+    elseif T1(i) >= 1 <= 10
+        x0 = 3*ones(4, 1);
+    else
+        x0 = [T1(i); 1; -T1(i)/1; 1]
     end
     
     [xopt, fopt, flag, iter] = fminsearch(@PIDOptim, x0, options);
